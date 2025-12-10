e8eb019d92c6a5f85a1e5d8b73838ed70fb28cb2
Sherif-toiema22 <sherif.toiema22@gmail.com>
2025-12-10 17:01:31 +0200

    Adding Controller layer

--- src/main/java/com/sherif/Docker/Spring/Boot/controller/myController.java
+++ src/main/java/com/sherif/Docker/Spring/Boot/controller/myController.java
@@ -1,10 +1,13 @@
 package com.sherif.Docker.Spring.Boot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class myController {
-    @RequestMapping
+    @GetMapping
     public String name(){
         return "Hello from after military status";
     }
